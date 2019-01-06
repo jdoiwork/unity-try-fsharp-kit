0a520c13f3e99db8886e2d9179073d262ac1becd
jdoi <jdoi.work@gmail.com>
2019-01-06 17:28:02 +0900

    1st commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,15 @@
+namespace FSharpProject
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpProject")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+()
+
---
+++ Assets/Scripts/HelloFsharp.fs
@@ -0,0 +1,6 @@
+﻿namespace FSharp
+open UnityEngine
+
+type HelloFsharp() =
+    inherit MonoBehaviour()
+    member this.Start() = Debug.Log("Hello World! jdoi2");
